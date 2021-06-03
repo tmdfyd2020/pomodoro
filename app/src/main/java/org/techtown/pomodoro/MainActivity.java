4346197d255e86d12b8c0fe1c2aa16b044d855ef
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2021-06-03 18:22:19 +0900

    툴바 아이콘 생성 및 화면 전환
    툴바 아이콘 생성 및 화면 전환, 화면 전환 후 다시 액티비티로 복귀 안전성 확인

--- app/src/main/java/org/techtown/pomodoro/MainActivity.java
+++ app/src/main/java/org/techtown/pomodoro/MainActivity.java
@@ -1,17 +1,18 @@
 package org.techtown.pomodoro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.Switch;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.tabs.TabLayout;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -80,6 +81,12 @@ public class MainActivity extends AppCompatActivity {
         });
         // #########################################################################################
 
+        // Topic 3 : 툴바(상단 메뉴바)에 아이콘 추가 및 화면 이동 ####################################
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // ActionBar actionbar = getSupportActionBar();
+        // #########################################################################################
+
     }
 
     // Topic 1 : 현재 시각 실시간 출력 ##############################################################
@@ -100,6 +107,32 @@ public class MainActivity extends AppCompatActivity {
     }
     // #############################################################################################
 
+    // Topic 3 : 툴바(상단 메뉴바)에 아이콘 추가 및 화면 이동 #########################################
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_weekly:
+                Intent weeklyIntent = new Intent(this, Weekly.class);
+                startActivity(weeklyIntent);
+                break;
+
+            case R.id.menu_settings:
+                Intent settingIntent = new Intent(this, Settings.class);
+                startActivity(settingIntent);
+                break;
+        }
+
+        return true;
+    }
+    // #############################################################################################
+
     @Override
     protected void onDestroy() {
         realtimeTimer.cancel();
@@ -115,6 +148,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         MainTimerTask timerTask = new MainTimerTask();
+        realtimeTimer = new Timer();  // 타이머를 화면 이동시 새로 생성해주지 않으면 오류
         realtimeTimer.schedule(timerTask, 500, 3000);
         super.onResume();
     }
---
+++ app/src/main/java/org/techtown/pomodoro/Settings.java
@@ -0,0 +1,26 @@
+package org.techtown.pomodoro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Settings extends AppCompatActivity {
+
+    Button ReturnBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        ReturnBtn = findViewById(R.id.ReturnBtn);
+        ReturnBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();  // 여기서 돌아갈 때 처리 다시 해 줘야 함
+            }
+        });
+    }
+}
